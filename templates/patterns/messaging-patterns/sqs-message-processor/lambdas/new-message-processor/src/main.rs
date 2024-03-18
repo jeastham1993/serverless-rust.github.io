0331955927d25b40b75c84310847129b6cf9fd10
James Eastham <dev@jameseastham.co.uk>
2024-03-18 17:06:52 +0000

    Add SQS example

---
+++ templates/patterns/messaging-patterns/sqs-message-processor/lambdas/new-message-processor/src/main.rs
@@ -0,0 +1,43 @@
+use aws_lambda_events::sqs::{BatchItemFailure, SqsBatchResponse, SqsEvent};
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+use shared::{InternalSqsMessage, MessageParseError, NewMessage, NewMessageHandler};
+
+async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<SqsBatchResponse, Error> {
+    let mut batch_item_failures = Vec::new();
+
+    for message in event.payload.records {
+        let new_message: Result<NewMessage, MessageParseError> = InternalSqsMessage::new(message.clone()).try_into();
+
+        if new_message.is_err() {
+            batch_item_failures.push(BatchItemFailure{
+                item_identifier: message.clone().message_id.unwrap()
+            });
+            continue;
+        }
+
+        // Business logic goes here
+        let handle_result = NewMessageHandler::handle(&new_message.unwrap()).await;
+
+        if handle_result.is_err() {
+            batch_item_failures.push(BatchItemFailure{
+                item_identifier: message.clone().message_id.unwrap()
+            });
+            continue;
+        }
+    }
+
+    Ok(SqsBatchResponse{
+        batch_item_failures
+    })
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .without_time()
+        .init();
+
+    run(service_fn(function_handler)).await
+}
